8c5fd630187ecf6c606d57735425acdd34c38f27
gabor_hanacsek <gabor_hanacsek@bud1eunb664420.eu.sanm.corp>
2020-06-18 14:31:23 +0200

    javitva a stringek felvitele, special karakterek kikerülnek, ha muszakvezeto ad hozza tervet bekerul a nem tervezett gyartas komment

--- src/bt/JobStatusThread.java
+++ src/bt/JobStatusThread.java
@@ -43,7 +43,7 @@ public class JobStatusThread implements Runnable {
             }
 
             try {
-                jobok = jobok.replaceAll("[-+'.^:,]","");
+                jobok = jobok.replaceAll("[-+'.^ :,]","");
                 
             } catch (Exception e) {
             }
